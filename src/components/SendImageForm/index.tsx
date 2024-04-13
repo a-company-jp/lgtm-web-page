ced1acd02e400081b67f2cb838b103f4da670862
Ishigami100 <yamahaya5151@icloud.com>
2024-04-13 15:20:04 +0900

    ボタンのcssをつける

--- src/components/SendImageForm/index.tsx
+++ src/components/SendImageForm/index.tsx
@@ -7,9 +7,12 @@ import { useSendImageForm, IMAGE_ID } from '../../hooks/useSendImageForm';
 export const SendImageForm: React.FC = () => {
   const { fileInputRef, rest, ref, selectFile } = useSendImageForm();
 
+  const style = {
+    container: 'w-20 h-7  rounded-md bg-green-700 text-white flex justify-center items-center cursor-pointer hover:bg-green-600',
+};
   return (
-    <div onClick={selectFile} role="presentation">
-      画像を選択する
+    <div onClick={selectFile} role="presentation" className={`${style.container}`} >
+      +追加
       <InputImage fileInputRef={fileInputRef} refCallback={ref} id={IMAGE_ID} {...rest} />
     </div>
   );
