6455b55a29acf13e562064842dda457663d095b1
Ishigami100 <yamahaya5151@icloud.com>
2024-04-14 03:12:32 +0900

    プライバシーポリシーも追加

--- src/components/atoms/CopyrightPolicyButton/index.tsx
+++ src/components/atoms/CopyrightPolicyButton/index.tsx
@@ -24,10 +24,10 @@ const CopyrightPolicyButton = () => {
             type="button"
             onClick={onOpen}
         >
-        Copyright Disclaimer Policy
+        Policies
         </button>
         <Modal isOpen={isOpen} onClose={onClose} style={{ backgroundColor: 'white' }} duration={0.4}>
-            <ModalHeader>利用上の注意</ModalHeader>
+            <ModalHeader>免責事項&プライバシーポリシー</ModalHeader>
 
             <ModalBody>
                 <DiscList>
@@ -35,8 +35,10 @@ const CopyrightPolicyButton = () => {
                     <ListItem>本サービスを利用して生成された画像はインターネット上に公開されます。元画像の著作権や関連法規に注意してください。公序良俗に反する画像や違法な画像を作成しないでください。これらの画像、その他運営者が不適切と判断した画像は予告無しに削除することがあります。</ListItem>
                     <ListItem>過剰な数のリクエストを送信してサービスに負荷をかける行為はおやめください。</ListItem>
                     <ListItem>その他、悪質な利用方法が確認された場合、特定のご利用者様を予告無しにアクセス禁止にすることがあります。</ListItem>
+                    <ListItem>プライバシーポリシー変更について当サイトは、個人情報に関して適用される日本の法令を遵守するとともに、本ポリシーの内容を適宜見直しその改善に努めます。修正された最新のプライバシーポリシーは常に本ページにて開示されます。</ListItem>
                 </DiscList>
             </ModalBody>
+
 
             <ModalFooter>
                 <Button variant="ghost" onClick={onClose}>
