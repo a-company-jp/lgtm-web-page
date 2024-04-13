0e2f28249b2651a364b5656ba51e51039738a7f1
Najah7 <najahmatsuo7@gmail.com>
2024-04-13 18:10:14 +0900

    💄 [Fix] Card component a little bit

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -15,9 +15,9 @@ const Card = ({ imageUri, title }: CardProps) => {
     // TODO: refactor css
     const styles = {
         centering: 'flex justify-center items-center',
-        card: 'w-full h-full',
-        linkWrapper: 'group relative block bg-black w-full h-full justify-between',
-        image: 'absolute inset-0 h-full w-full  opacity-75 transition-opacity group-hover:opacity-50',
+        card: 'w-full h-full rounded-xl',
+        linkWrapper: 'group relative block bg-black w-full h-full justify-between rounded-xl',
+        image: 'absolute inset-0 h-full w-full  opacity-75 transition-opacity group-hover:opacity-50 rounded-xl',
         title: 'text-xl font-bold text-white sm:text-2xl top-4',
         hoverBox: 'mt-20',
         copyBox: 'translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100 mt-4',
--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -8,7 +8,7 @@ function CardList(props: CardListProps) {
     const cardDataArray = Object.values(props)
 
     const styles = {
-        grid: 'grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8 md:grid-cols-2 md:gap-4 sm:grid-cols-2 sm:gap-4',
+        grid: 'grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8 md:grid-cols-2 md:gap-4 sm:grid-cols-2 sm:gap-4 mt-4 mx-6 lg:mx-36',
         gridItem: 'h-56 rounded-lg',
     }
 
