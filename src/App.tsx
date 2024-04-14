f894b224441c44998b322444ffca8c317b23a71f
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-14 14:02:56 +0900

    ✨ バナー追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import Header from './components/Header';
-import './App.css';
-import CardList from './components/CardList';
-import SendImageButton from './components/SendImageButton';
-import { UIProvider } from "@yamada-ui/react"
-import { Pagination } from './components/Pagination';
-import { useState } from 'react';
-import useLGTMFetch from './hooks/useLGTMFetch';
+import Header from "./components/Header";
+import "./App.css";
+import CardList from "./components/CardList";
+import SendImageButton from "./components/SendImageButton";
+import { UIProvider } from "@yamada-ui/react";
+import { Pagination } from "./components/Pagination";
+import { useState } from "react";
+import useLGTMFetch from "./hooks/useLGTMFetch";
+import Banner from "./components/Banner";
 
 function App() {
-
   const [activePage, setActivePage] = useState(1);
   const [uploaded, setUploaded] = useState(false);
-  const {LGTMUrls} = useLGTMFetch(activePage, uploaded, setUploaded);
+  const { LGTMUrls } = useLGTMFetch(activePage, uploaded, setUploaded);
 
   return (
     <UIProvider>
       <div className="App">
+        <Banner />
         <Header />
         <SendImageButton setUploaded={setUploaded} />
         <CardList {...LGTMUrls} />
