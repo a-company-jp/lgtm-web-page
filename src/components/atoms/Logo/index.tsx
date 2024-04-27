db5db7dabda6b6d94966bbb5eded4bd315a1da48
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-28 02:59:38 +0900

    💄 logo update

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <>
-      <img src="LGTM_logo.png" alt="" className="w-72 h-48 my-10" />
+      <img src="LGTM_logo.png" alt="" className="w-72 h-36 my-10" />
     </>
   );
 };
