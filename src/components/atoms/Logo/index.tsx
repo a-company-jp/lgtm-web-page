a012a756f822a3397d272cc89020eeb8ca3ecac6
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-27 10:17:45 +0900

    ✨ Logo-Componentの作成

---
+++ src/components/atoms/Logo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <>
+      <img src="LGTM_logo.png" alt="" className="w-72 h-48 my-10" />
+    </>
+  );
+};
+
+export default Logo;
